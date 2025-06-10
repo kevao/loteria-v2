5cbca2ef82f9fd0eeb0dd402fd8dc20bb84b1c97
Kevin Villanova <kevin@alpina.digital>
2025-06-10 09:53:24 -0300

    Interface de serviço

---
+++ src/main/java/dev/loteria/interfaces/Servico.java
@@ -0,0 +1,13 @@
+package dev.loteria.interfaces;
+
+public interface Servico {
+  public void inserir();
+
+  public void listar();
+
+  public void atualizar();
+
+  public void deletar();
+
+  public void retornarMenu();
+}
