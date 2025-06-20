907e455611d574fca75cf592dfd866dfbd56b552
Kevin Villanova <kevin@alpina.digital>
2025-06-20 18:49:40 -0300

    CRUD modelo

--- src/main/java/dev/loteria/interfaces/CRUD.java
+++ src/main/java/dev/loteria/interfaces/CRUD.java
@@ -2,12 +2,12 @@ package dev.loteria.interfaces;
 
 import java.sql.ResultSet;
 
-public interface CRUD<T> {
+public interface CRUD<TModelo> {
   public ResultSet listar();
 
-  public void inserir(T modelo);
+  public void inserir(TModelo modelo);
 
-  public void editar(T modelo);
+  public void editar(TModelo modelo);
 
   public void deletar(int id);
 }
